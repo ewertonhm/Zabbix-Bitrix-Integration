86f6ea54993b9c3103a4b6841ef09272450808c0
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2021-10-25 03:48:46 -0300

    Add files via upload

---
+++ controller/insert.sql
@@ -0,0 +1,84 @@
+INSERT INTO usuario (
+    email,
+    senha,
+    nome,
+    admin
+  )
+VALUES (
+    'ewerton.marschalk@redeunifique.com.br',
+    '241f47d048d825ffea7ea56aacf02653',
+    'Ewerton',
+    1
+  );
+
+INSERT INTO api_keys (
+  api_key
+)
+VALUES (
+  '262e8070d7d4157dfc01685f3b200b26'
+);
+
+INSERT INTO unidade (nome, sigla) VALUES ('Agrolândia/Ituporanga', 'TECH');
+INSERT INTO unidade (nome, sigla) VALUES ('Videira', 'VII');
+INSERT INTO unidade (nome, sigla) VALUES ('Benedito Novo', 'INT');
+INSERT INTO unidade (nome, sigla) VALUES ('Bombinhas', 'BOMB');
+INSERT INTO unidade (nome, sigla) VALUES ('Brusque', 'JGM');
+INSERT INTO unidade (nome, sigla) VALUES ('Caçador', 'CON');
+INSERT INTO unidade (nome, sigla) VALUES ('Canoinhas', 'SULCNI');
+INSERT INTO unidade (nome, sigla) VALUES ('Chapecó', 'CCO');
+INSERT INTO unidade (nome, sigla) VALUES ('Cocal do Sul', 'INFO');
+INSERT INTO unidade (nome, sigla) VALUES ('Concórdia', 'CDA');
+INSERT INTO unidade (nome, sigla) VALUES ('Corupá', 'RSCOM');
+INSERT INTO unidade (nome, sigla) VALUES ('Fraiburgo', 'HBINFO');
+INSERT INTO unidade (nome, sigla) VALUES ('Garopaba', 'SONIC/SNET');
+INSERT INTO unidade (nome, sigla) VALUES ('Guabiruba', 'GBA');
+INSERT INTO unidade (nome, sigla) VALUES ('Itajaí', 'SULIAI');
+INSERT INTO unidade (nome, sigla) VALUES ('Ilhota', 'JGM');
+INSERT INTO unidade (nome, sigla) VALUES ('Itaiópolis', 'BKTECH');
+INSERT INTO unidade (nome, sigla) VALUES ('Ituporanga', 'TECH');
+INSERT INTO unidade (nome, sigla) VALUES ('Jaraguá do Sul', 'TFI');
+INSERT INTO unidade (nome, sigla) VALUES ('Joaçaba', 'JCA');
+INSERT INTO unidade (nome, sigla) VALUES ('Joinville', 'SULJVE');
+INSERT INTO unidade (nome, sigla) VALUES ('LPA', 'Lages');
+INSERT INTO unidade (nome, sigla) VALUES ('Luiz Alves', 'ANET');
+INSERT INTO unidade (nome, sigla) VALUES ('Nova Veneza', 'INFO');
+INSERT INTO unidade (nome, sigla) VALUES ('Pomerode', 'CNT');
+INSERT INTO unidade (nome, sigla) VALUES ('Rio do Sul', 'BRL');
+INSERT INTO unidade (nome, sigla) VALUES ('São Bento do Sul', 'WBT');
+INSERT INTO unidade (nome, sigla) VALUES ('São João Batista', 'GBA');
+INSERT INTO unidade (nome, sigla) VALUES ('São Joaquim', 'CNS');
+INSERT INTO unidade (nome, sigla) VALUES ('São José', 'GWT');
+INSERT INTO unidade (nome, sigla) VALUES ('Schroeder / Barra Velha', 'RVIP');
+INSERT INTO unidade (nome, sigla) VALUES ('Timbó', 'TPA');
+INSERT INTO unidade (nome, sigla) VALUES ('Tijucas', 'INFOBY');
+INSERT INTO unidade (nome, sigla) VALUES ('Videira', 'VII');
+INSERT INTO unidade (nome, sigla) VALUES ('Xaxim', 'TECNET');
+
+INSERT INTO tecnologia(tecnologia) VALUES ('Rádio');
+INSERT INTO tecnologia(tecnologia) VALUES ('Fibra');
+INSERT INTO tecnologia(tecnologia) VALUES ('Infra');
+INSERT INTO tecnologia(tecnologia) VALUES ('Coaxial');
+INSERT INTO tecnologia(tecnologia) VALUES ('Desenvolvimento');
+INSERT INTO tecnologia(tecnologia) VALUES ('Telefonia');
+INSERT INTO tecnologia(tecnologia) VALUES ('IDC - Emails');
+INSERT INTO tecnologia(tecnologia) VALUES ('IDC - SW');
+INSERT INTO tecnologia(tecnologia) VALUES ('IDC - VM/Hosts');
+INSERT INTO tecnologia(tecnologia) VALUES ('Protheus');
+INSERT INTO tecnologia(tecnologia) VALUES ('CGR - Switching');
+INSERT INTO tecnologia(tecnologia) VALUES ('CGR - Routing');
+INSERT INTO tecnologia(tecnologia) VALUES ('CGR - Rádio');
+
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Alan Carlos Becker', '727');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Daniel Henrique Withoeft', '729');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Deivid Patrick', '731');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Fagner Gustavo Agostini', '733');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Guilherme Damke', '735');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Welynthon Augusto Suptitz', '1257');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Sergio Luis Fiamoncini', '1303');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Bruno Rafael Souza', '2963');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Ewerton Henrique Marschalk', '2831');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Lucas Schelbauer', '2871');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Lucas de Lima', '3459');
+INSERT INTO n3_accomplice (nome, bitrix_id) VALUES ('Luiz Eduardo Krol Pereira', '5111');
+
+
